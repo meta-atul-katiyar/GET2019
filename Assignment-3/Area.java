23c4ad9d9097ddc8b4015dc9afa6075bc08eb9b8
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-15 04:05:24 -0700

    Update Area.java

--- Assignment-3/Area.java
+++ Assignment-3/Area.java
@@ -2,15 +2,8 @@
 public class Area { 
 	static double PI = 3.14;
 	//area of triangle
-	public static double areaTriangle(double side1, double side2, double side3) {
-		if((side1+side2)>side3 && (side1+side3)>side2 && (side2+side3)>side1){
-   	  		double s=(side1+side2+side3)2;
-        		double  area=Math.sqrt(s(s-side1)(s-side2)(s-side3));
-        		return area; 
-        	}
-    		else  
-        		System.out.println(Area of Triangle not exist);		
-		return 0;
+	public static double areaTriangle(double base, double height) {
+		return base*height/2;
 	}
 	//area of rectangle
 	public static double areaRectangle(double side1, double side2) {
@@ -18,7 +11,7 @@ public class Area {
 	}
 	//area of square
 	public static double areaSquare(double side) {
-		return sideside;
+		return side*side;
 	}
 	//area of circle
 	public static double areaCircle(double radius) {
