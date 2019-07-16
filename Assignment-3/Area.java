7d9419fb91121ef7ee1331b60bcabfd570f044be
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-16 02:14:24 -0700

    Update Area.java

--- Assignment-3/Area.java
+++ Assignment-3/Area.java
@@ -3,15 +3,15 @@ public class Area {
 	static double PI = 3.14;
 	//area of triangle
 	public static double areaTriangle(double base, double height) {
-		return base*height/2;
+		return (base*height/2);
 	}
 	//area of rectangle
 	public static double areaRectangle(double side1, double side2) {
-		return side1*side2; 
+		return (side1*side2); 
 	}
 	//area of square
 	public static double areaSquare(double side) {
-		return side*side;
+		return (side*side);
 	}
 	//area of circle
 	public static double areaCircle(double radius) {
