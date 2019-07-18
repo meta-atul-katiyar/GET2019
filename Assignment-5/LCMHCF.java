d9bbdf824e8f3ec419cc6a008b9cc37c9ffa17b7
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-18 21:45:06 +0530

    Update LCMHCF.java

--- Assignment-5/LCMHCF.java
+++ Assignment-5/LCMHCF.java
@@ -2,26 +2,16 @@
 public class LCMHCF {
 	//return HCF of Two numbers recursively
 	public static int HCF(int x, int y){
-		if(y == 0){
+		if(y==0){
 			return x;
 		}
-		if(x > y){
-			int diff = x-y;
-			if(diff>y){
-				return HCF(diff,y);
-			}
-			else{
-				return HCF(y,diff);
-			}
+		if(x>y){
+			int mod = x%y;
+			return lcm(y, mod);
 		}
 		else{
-			int diff = y-x;
-			if(diff>y){
-				return HCF(diff,x);
-			}
-			else{
-				return HCF(x, diff);
-			}
+			int mod = y%x;
+			return lcm(x,mod);
 		}
 	}
 	// return LCM of Two numbers recursively
