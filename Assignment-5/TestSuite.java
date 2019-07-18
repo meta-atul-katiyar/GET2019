d9130bcbd231b8f3db494a2d450a667c451b3e78
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-18 04:38:45 -0700

    Create TestSuite.java

---
+++ Assignment-5/TestSuite.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;		
+import org.junit.runners.Suite;	
+@RunWith(Suite.class)				
+@Suite.SuiteClasses({
+	LCMHCFTest.class,
+	NQueenTest.class,
+	SearchTest.class
+})
+public class TestSuite {
+
+}
