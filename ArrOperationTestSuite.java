27dd699a4021c344e5f9ccdac9d1b4e8f0e8f011
meta-atul-katiyar <atul.katiyar@metacube.com>
2019-07-18 03:16:58 -0700

    test suite

---
+++ ArrOperationTestSuite.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;		
+import org.junit.runners.Suite;	
+@RunWith(Suite.class)				
+@Suite.SuiteClasses({
+	CountClumpsTest.class,
+	MaxMirrorTest.class,
+	SplitArrayTest.class,
+	FixXYTest.class,
+	NegativeTest.class
+	
+})
+public class ArrOperationTestSuite {
+
+}
