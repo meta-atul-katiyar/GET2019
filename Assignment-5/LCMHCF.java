10e049a8f53590ff343fc8fef0e67b554b68fcfc
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-19 01:21:18 -0700

    Update LCMHCF.java

--- Assignment-5/LCMHCF.java
+++ Assignment-5/LCMHCF.java
@@ -1,22 +1,31 @@
 
 public class LCMHCF {
 	//return HCF of Two numbers recursively
-	public static int HCF(int x, int y){
-		if(y==0){
-			return x;
+	public static int hcfLCMInput(int num1, int num2){
+		if(num1==0 || num2==0){
+			return 0;
 		}
-		else{
-			int mod = x%y;
-			return HCF(x,mod);
+		else {
+			return HCF(num1,num2);
 		}
+	}
+	public static int HCF(int num1, int num2){
+		if(num1<0 || num2<0){
+			throw new AssertionError("number must be positive.");
+		}
+		if(num2 == 0){
+			return num1;
+		}
+		int mod = num1%num2;
+		return HCF(num2, mod);
 	}
 	// return LCM of Two numbers recursively
 	public static int LCM(int x, int y){
-		int hcf = HCF(x,y);
+		int hcf = hcfLCMInput(x,y);
 		return (x*y/hcf);
 	}
 	
 	public static void main(String[] args){
-		System.out.print(LCM(49,10));
+		System.out.print(LCM(10, 4));
 	}
 }
