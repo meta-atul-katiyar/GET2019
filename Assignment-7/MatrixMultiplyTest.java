d2b0823f784344a1817ce8f88ad7cbde8662e7fe
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-23 04:41:03 -0700

    Update MatrixMultiplyTest.java

--- Assignment-7/MatrixMultiplyTest.java
+++ Assignment-7/MatrixMultiplyTest.java
@@ -4,27 +4,42 @@ import org.junit.Test;
 import org.junit.Before;
 
 public class MatrixMultiplyTest {
-	private int[][] expected = new int[][] {{1,1,10},{4,4,12}};
+	private int[][] expected = new int[][] {
+			{1, 1, 10},
+			{5, 1, 30},
+			{4, 5, 20},
+			{0, 2, 3},
+			{0, 4, 4},
+			{2, 0, 9}
+			};
 	private SparseMatrices sparseMatrix1;
 	private SparseMatrices sparseMatrix2;
 	
 	 @Before
 	 public void initialize() {
-		int[] row = new int[]   {0,1,4,3,4,5};
-		int[] column = new int[]{0,1,0,3,4,5};
-		int[] value = new int[] {1,2,3,4,5,6};
-			
-		int[] row1 = new int[]   {0,0,1,4,5,2};
-		int[] column1 = new int[]{2,4,1,5,1,0};
-		int[] value1 = new int[] {3,4,5,4,5,3};
+		 int[][] matrix = new int[][] {
+					{0,0,1},
+					{1,1,2},
+					{2,2,3},
+					{3,3,4},
+					{4,4,5},
+					{5,5,6}
+			};
+			int [][] matrix1 = new int[][]{
+					{0,2,3},
+					{0,4,4},
+					{1,1,5},
+					{4,5,4},
+					{5,1,5},
+					{2,0,3}
+			};
 		
-		sparseMatrix1 = new SparseMatrices(row, column, value);
-		sparseMatrix2 = new SparseMatrices(row1, column1, value1);
+		sparseMatrix1 = new SparseMatrices(matrix);
+		sparseMatrix2 = new SparseMatrices(matrix1);
 	 }
 	 
 	 @Test
 	 public void testLCMHCF() {
-		 //System.out.println("ans: "+poly.evaluate((float)2.7));
 	     assertArrayEquals(expected, 
 	    		 SparseMatrices.multiplyMatrices(sparseMatrix1, sparseMatrix2));
 	   } 
