ea3a1dccfc8af2bc1862304888ccb6a854751dc4
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-23 04:36:01 -0700

    multiplication method

--- Assignment-7/SparseMatrices.java
+++ Assignment-7/SparseMatrices.java
@@ -1,40 +1,45 @@
 import java.util.*;
+
 public final class SparseMatrices {
-	final int[] row;
-	final int[] column;
-	final int[] value;
+	private final int immutableMatrix[][];
 	
-	public SparseMatrices(int[]arrRow, int[]arrColumn, int[]arrValue){
-		int arrRowLength = arrRow.length;// arrColumnLength = arrColumn.length, arrValueLength = arrValue.length;
-		if((arrRowLength == arrColumn.length) && ( arrRowLength== arrValue.length)){
-			this.row = arrRow;
-			this.column = arrColumn;
-			this.value = arrValue;
+	/**
+	 * @param matrix
+	 * 
+	 */
+	public SparseMatrices(int[][] matrix){
+		if(matrix.length == 0){
+			throw new AssertionError("null matrix");
 		}
-		else{
-			throw new AssertionError("not valid data.");
-		}
+		this.immutableMatrix = matrix;
 	}
 	
+	/**
+	 * @param object of class
+	 * @return transpose of matrix (as 2d matrix)
+	 */
 	public static int[][] transpose(SparseMatrices sm){
-		int valueLength = sm.value.length;
-		int[][] sparseMatrix = new int[3][valueLength];
+		int valueLength = sm.immutableMatrix.length;
 		for (int count=0; count < valueLength; count++){
-			int tempVar = sm.row[count];
-			sm.row[count] = sm.column[count];
-			sm.column[count] = tempVar;
+			int tempVar = sm.immutableMatrix[count][0];
+			sm.immutableMatrix[count][0] = sm.immutableMatrix[count][1];
+			sm.immutableMatrix[count][1] = tempVar;
 			
-			System.out.println(sm.row[count]+" "+sm.column[count]+" "+sm.value[count]);
+			System.out.println(sm.immutableMatrix[count][0]+" "+sm.immutableMatrix[count][1]+" "+sm.immutableMatrix[count][2]);
 		}
-		return sparseMatrix;
+		return sm.immutableMatrix;
 	}
 	
+	/**
+	 * @param object of class
+	 * @return true if symmetric else false
+	 */
 	public static boolean isSymmetric(SparseMatrices sm){
-		int valueLength = sm.value.length, checkLength = 0;
+		int valueLength = sm.immutableMatrix.length, checkLength = 0;
 		for (int indexRow = 0; indexRow < valueLength; indexRow++){
 			for (int indexColumn = 0; indexColumn < valueLength; indexColumn++){
-				if(sm.row[indexRow] == sm.column[indexColumn] && sm.row[indexColumn] == sm.column[indexRow]){
-					if(sm.value[indexRow] == sm.value[indexColumn]){
+				if(sm.immutableMatrix[indexRow][0] == sm.immutableMatrix[indexColumn][1] && sm.immutableMatrix[indexRow][1] == sm.immutableMatrix[indexColumn][0]){
+					if(sm.immutableMatrix[indexRow][2] == sm.immutableMatrix[indexColumn][2]){
 						checkLength += 1;
 						System.out.println(checkLength);
 					}
@@ -47,83 +52,108 @@ public final class SparseMatrices {
 		return false;
 	}
 	
+	/**
+	 * @param object of class
+	 * @return matrix size and number of entries in sparse matrix
+	 */
 	private static int[] matrixSize(SparseMatrices sm){
 		int[] arr = new int[]{0,0,0};
-		arr[2] = sm.value.length;
+		arr[2] = sm.immutableMatrix.length;
 		for (int indexRow = 0; indexRow < arr[2]; indexRow++){
-			if(sm.row[indexRow] > arr[0]){
-				arr[0] = sm.row[indexRow];
+			if(sm.immutableMatrix[indexRow][0] > arr[0]){
+				arr[0] = sm.immutableMatrix[indexRow][0];
 			}
-			if(sm.column[indexRow] > arr[1]){
-				arr[1] = sm.column[indexRow];
+			if(sm.immutableMatrix[indexRow][1] > arr[1]){
+				arr[1] = sm.immutableMatrix[indexRow][1];
 			}
 		}
 		return arr;
 	}
+	
+	/**
+	 * @param sparse matrix as HashMap with HashMap<Integer[], Integer>
+	 * @return sparse matrix as 2d array
+	 */
+	private static int[][] hashmapToArray(HashMap<Integer[], Integer> map){
+		int [][] array = new int[map.size()][3];
+		int index = 0;
+		for(Integer[] key : map.keySet()){
+			array[index][0] = key[0];
+			array[index][1] = key[1];
+			array[index][2] = map.get(key);
+			index += 1;
+		}
+		return array;
+	}
+	
+	/**
+	 * @param class object
+	 * @param class object
+	 * @return sum of two sparse matrix
+	 */
 	public static int[][] addTwoMatrices(SparseMatrices sm1, SparseMatrices sm2){
 		int	newLength = 0;
 		int [] matrixParam1 = matrixSize(sm1);
 		int [] matrixParam2 = matrixSize(sm2);
-		System.out.println("atul "+matrixParam1[0]+" "+matrixParam2[0]);
-		LinkedList <Integer> rowList = new LinkedList <Integer> ();
-		LinkedList <Integer> columnList = new LinkedList <Integer> ();
-		LinkedList <Integer> valueList = new LinkedList <Integer> ();
+		HashMap<Integer[], Integer> map = new HashMap<>();
 		if(matrixParam1[0] == matrixParam2[0] && matrixParam1[1] == matrixParam2[1]){
 			for (int indexRow = 0; indexRow < matrixParam1[2]; indexRow++){
 				for (int indexColumn = 0; indexColumn < matrixParam2[2]; indexColumn++){
-					if(sm1.row[indexRow] == sm2.row[indexColumn] && sm2.column[indexColumn] == sm1.column[indexRow]){
-						rowList.add(sm1.row[indexRow]);
-						columnList.add(sm1.column[indexRow]);
-						valueList.add(sm1.value[indexRow] + sm2.value[indexColumn]);
+					if(sm1.immutableMatrix[indexRow][0] == sm2.immutableMatrix[indexColumn][0] && 
+							sm2.immutableMatrix[indexColumn][1] == sm1.immutableMatrix[indexRow][1]){
+						
+						Integer[] key = new Integer[] {sm1.immutableMatrix[indexRow][0],
+								sm1.immutableMatrix[indexRow][1]};
+						map.put(key, sm1.immutableMatrix[indexRow][2]+sm2.immutableMatrix[indexRow][2]);
 						indexRow += 1;
 						indexColumn = 0;
 						newLength += 1;
 						
 					}
 					else if(indexColumn == matrixParam2[2]-1) {
-						rowList.add(sm1.row[indexRow]);
-						columnList.add(sm1.column[indexRow]);
-						valueList.add(sm1.value[indexRow]);
+						Integer[] key = new Integer[] {sm1.immutableMatrix[indexRow][0],
+								sm1.immutableMatrix[indexRow][1]};
+						map.put(key, sm1.immutableMatrix[indexRow][2]);
 						
-						rowList.add(sm2.row[indexColumn]);
-						columnList.add(sm2.column[indexColumn]);
-						valueList.add(sm2.value[indexColumn]);
+						key = new Integer[] {sm2.immutableMatrix[indexRow][0],
+								sm2.immutableMatrix[indexRow][1]};
+						map.put(key, sm2.immutableMatrix[indexRow][2]);
 						
 						newLength += 2;
 					}
 				}
-				//System.out.println(newLength);
 			}
 		}
 		else{
 			throw new AssertionError("addition of these matrices not possible.");
 		}
-		int array[][] = new int [3][newLength];
-		for(int columnNum = 0; columnNum < newLength; columnNum++){
-			array[0][columnNum] = rowList.removeFirst();
-			array[1][columnNum] = columnList.removeFirst();
-			array[2][columnNum] = valueList.removeFirst();
-			System.out.println(array[0][columnNum]+" "+array[1][columnNum]+" "+array[2][columnNum]);
-		}
-		return array;
+		return hashmapToArray(map);
 	}
 	
+	/**
+	 * @param class object
+	 * @param class object
+	 * @return multiplication of sparse matrix
+	 */
 	public static int[][] multiplyMatrices(SparseMatrices sm1, SparseMatrices sm2){
 		int	newLength = 0;
 		int [] matrixParam1 = matrixSize(sm1);
 		int [] matrixParam2 = matrixSize(sm2);
-		
-		LinkedList <Integer> rowList = new LinkedList <Integer> ();
-		LinkedList <Integer> columnList = new LinkedList <Integer> ();
-		LinkedList <Integer> valueList = new LinkedList <Integer> ();
+
+        HashMap<Integer[], Integer> map = new HashMap<>();
 		if(matrixParam1[1] == matrixParam2[1]){
 			for (int indexRow = 0; indexRow < matrixParam1[2]; indexRow++){
 				for (int indexColumn = 0; indexColumn < matrixParam2[2]; indexColumn++){
-					if(sm1.row[indexRow] == sm2.column[indexColumn] && sm2.row[indexColumn] == sm1.column[indexRow]){
-						rowList.add(sm1.row[indexRow]);
-						columnList.add(sm2.column[indexColumn]);
-						valueList.add(sm1.value[indexRow] * sm2.value[indexColumn]);
-						newLength += 1;
+					if(sm2.immutableMatrix[indexColumn][0] == sm1.immutableMatrix[indexRow][1]){
+						Integer arr[]= new Integer[]{sm1.immutableMatrix[indexRow][0],
+								sm2.immutableMatrix[indexColumn][1]};
+						int num = sm1.immutableMatrix[indexRow][2] * sm2.immutableMatrix[indexColumn][2];
+						if(map.get(arr) == null){
+							map.put(arr, num);
+						}
+						else{
+							map.replace(arr, map.get(arr)+num);
+						}
 					}
 				}
 			}
@@ -131,31 +161,34 @@ public final class SparseMatrices {
 		else{
 			throw new AssertionError("multiplication of these matrices not possible.");
 		}
-		int array[][] = new int [3][newLength];
-		for(int columnNum = 0; columnNum < newLength; columnNum++){
-			array[0][columnNum] = rowList.removeFirst();
-			array[1][columnNum] = columnList.removeFirst();
-			array[2][columnNum] = valueList.removeFirst();
-			System.out.println(array[0][columnNum]+" "+array[1][columnNum]+" "+array[2][columnNum]);
-		}
-		return array;
+		return hashmapToArray(map);
 	}
 	
 	public static void main(String[] args){
-		int[] row = new int[]   {0,1,4,3,4,5};
-		int[] column = new int[]{0,1,0,3,4,5};
-		int[] value = new int[] {1,2,3,4,5,6};
+		int[][] matrix = new int[][] {
+				{0,0,1},
+				{1,1,2},
+				{2,2,3},
+				{3,3,4},
+				{4,4,5},
+				{5,5,6}
+		};
+		int [][] matrix1 = new int[][]{
+				{0,2,3},
+				{0,4,4},
+				{1,1,5},
+				{4,5,4},
+				{5,1,5},
+				{2,0,3}
+		};
 		
-		int[] row1 = new int[]   {0,0,1,4,5,2};
-		int[] column1 = new int[]{2,4,1,5,1,0};
-		int[] value1 = new int[] {3,4,5,4,5,3};
-		
-		SparseMatrices sm1 = new SparseMatrices(row, column, value);
-		SparseMatrices sm2 = new SparseMatrices(row1, column1, value1);
-		//int[][] arr= transpose(sm1);
-		int[][] arr1 = multiplyMatrices(sm1,sm2);
-		//System.out.println(isSymmetric(sm1));
-		
+		SparseMatrices sm1 = new SparseMatrices(matrix);
+		SparseMatrices sm2 = new SparseMatrices(matrix1);
+		int[][] arr = multiplyMatrices(sm1,sm2);
+		int length = arr.length;
+		System.out.println("multiplication:");
+		for(int i =0; i<length; i++){
+			System.out.println(arr[i][0]+" "+arr[i][1]+" "+arr[i][2]);
+		}	
 	}
-	
 }
