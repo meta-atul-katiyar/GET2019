b64c9cb1e63844867cb624011d443a93cac86a37
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-25 03:13:59 -0700

    Update Cage.java

--- Assignment-8/Cage.java
+++ Assignment-8/Cage.java
@@ -1,14 +1,21 @@
+import java.util.LinkedList;
 
 public class Cage {
 	int id;
 	String animalType;
 	static int capacity;
 	static int count = 0;
+	static LinkedList<Integer> animals = new LinkedList<Integer>();
+	
 	
 	public Cage(int capacity, String animalType){
 		this.id = 210000 + count++;
 		this.capacity = capacity;
 		this.animalType = animalType;
+	}
+	public void addAnimal(int id){
+		this.animals.add(id);
+		this.capacity -= 1;
 	}
 	
 }
