675f8959c2e8a421cce78794146a932af1b6c251
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-25 03:16:24 -0700

    Update Reptiles.java

--- Assignment-8/Reptiles.java
+++ Assignment-8/Reptiles.java
@@ -1,15 +1,18 @@
 import java.util.List;
 
 public abstract class Reptiles extends Animal {
-	private static List<Zone> zones;
-	
 	Reptiles(float age, float weight, String gender) {
 		super(age, weight, gender);
 		
 	}
-
-	public void addZone(int capacity){
-		Zone zone = new Zone(capacity);
-		zones.add(zone);
+	@Override
+	public boolean isWarmBlooded() {
+		return false;
 	}
+
+	@Override
+	public boolean isColdBlooded() {
+		return true;
+	}
+
 }
