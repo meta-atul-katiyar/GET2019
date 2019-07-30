e9299f91c066d0c803180fea47d00818e3e69594
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-07-30 01:47:00 -0700

    Update MultivariatePoly.java

--- DS-1/MultivariatePoly.java
+++ DS-1/MultivariatePoly.java
@@ -20,37 +20,32 @@ public class MultivariatePoly {
 		if(varPowArray.length != variableNum){
 			throw new AssertionError("invalid coefficient array.");
 		}
-		else{
-			Coefficient newCoefficient = new Coefficient(varPowArray, coeffValue);
-			if(mPoly.start == null){
-				Degree newDegree = new Degree(degree, newCoefficient);
-				mPoly.start = newDegree;
-			}
-			else{
-				Degree currDegree = mPoly.start;
-				int flag = 0;
-				while(currDegree.data != degree){
-					currDegree = currDegree.nextDegree;
-					if(currDegree == null){
-						flag = 1;
-						break;
-					}
-				}
-				if(flag == 0){
-					while(currDegree.nextCoeff.next != null){
-						currDegree.nextCoeff = currDegree.nextCoeff.next;
-					}
-					currDegree.nextCoeff.next = newCoefficient;
-				}
-				else{currDegree = mPoly.start;
-					while(currDegree.nextDegree != null){
-						currDegree = currDegree.nextDegree;
-					}
-					Degree newDegree = new Degree(degree, newCoefficient);
-					currDegree.nextDegree = newDegree;
-				}
-			}	
+		Coefficient newCoefficient = new Coefficient(varPowArray, coeffValue);
+		if(mPoly.start == null){
+			Degree newDegree = new Degree(degree, newCoefficient);
+			mPoly.start = newDegree;
 		}
+		Degree currDegree = mPoly.start;
+		int flag = 0;
+		while(currDegree.data != degree){
+			currDegree = currDegree.nextDegree;
+			if(currDegree == null){
+				flag = 1;
+				break;
+			}
+		}
+		if(flag == 0){
+			while(currDegree.nextCoeff.next != null){
+				currDegree.nextCoeff = currDegree.nextCoeff.next;
+			}
+			currDegree.nextCoeff.next = newCoefficient;
+		}
+		currDegree = mPoly.start;
+		while(currDegree.nextDegree != null){
+			currDegree = currDegree.nextDegree;
+		}
+		Degree newDegree = new Degree(degree, newCoefficient);
+		currDegree.nextDegree = newDegree;
 		return mPoly;
 	}
 
@@ -62,23 +57,21 @@ public class MultivariatePoly {
 		if(mPoly.start == null){
 			throw new AssertionError("empty.");
 		}
-		else{
-			Degree currDegree = mPoly.start;
-			System.out.println("linked list:");
-			while(currDegree != null){
-				System.out.print(currDegree.data + "\t");
-				while(currDegree.nextCoeff != null){
-					int [] coeffArray = currDegree.nextCoeff.varPowArray;
-					System.out.print(currDegree.nextCoeff.coeffValue+"[");
-					for(int count=0; count < coeffArray.length; count++){
-						System.out.print(coeffArray[count]+" ");
-					}
-					System.out.print("]\n\t");
-					currDegree.nextCoeff = currDegree.nextCoeff.next;
+		Degree currDegree = mPoly.start;
+		System.out.println("linked list:");
+		while(currDegree != null){
+			System.out.print(currDegree.data + "\t");
+			while(currDegree.nextCoeff != null){
+				int [] coeffArray = currDegree.nextCoeff.varPowArray;
+				System.out.print(currDegree.nextCoeff.coeffValue+"[");
+				for(int count=0; count < coeffArray.length; count++){
+					System.out.print(coeffArray[count]+" ");
 				}
-				System.out.println();
-				currDegree = currDegree.nextDegree;
+				System.out.print("]\n\t");
+				currDegree.nextCoeff = currDegree.nextCoeff.next;
 			}
+			System.out.println();
+			currDegree = currDegree.nextDegree;
 		}
 	}
 	
@@ -101,18 +94,5 @@ public class MultivariatePoly {
 			}
 		}
 		return polyDegree;
-	}
-	
-	public static void main(String[]args){
-		MultivariatePoly mPoly = new MultivariatePoly(4);
-		mPoly.insert(mPoly, -3, new int[]{2,2,0,0});
-		mPoly.insert(mPoly, 2,new int[]{2,2,2,2});
-		mPoly.insert(mPoly, -9,new int[]{2,0,2,0});
-		mPoly.insert(mPoly, 25, new int[]{0,0,0,0});
-		
-		mPoly.printMPoly(mPoly);
-		
-		System.out.println(degreeOfPoly(mPoly));
-		
 	}
 }
