57391591c146ac0d81c56ba5c641ccb4d7a932d1
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-08-03 03:18:41 -0700

    Update EmployNameSort.java

--- DS-4/P-1/EmployNameSort.java
+++ DS-4/P-1/EmployNameSort.java
@@ -6,8 +6,8 @@ public class EmployNameSort{
 	Set<Employ> employSet = new HashSet<Employ>();
 	LinkedList<Employ> sortedEmpList = new LinkedList<Employ>();
 
-	public void addToEmploy(String name, String address){
-		Employ employ = new Employ(name, address);
+	public void addToEmploy(int id, String name, String address){
+		Employ employ = new Employ(id, name, address);
 		employSet.add(employ);
 	}
 
@@ -28,15 +28,23 @@ public class EmployNameSort{
 				sortedEmpList.add(index, emp);
 			}
 		}
-		
+	}
+	
+	public String[] sortedNames(){
+		int empNum = sortedEmpList.size(), index = 0;
+		String [] empNames = new String[empNum];
+		for(Employ emp : this.sortedEmpList){
+			empNames[index++] = emp.getName();
+		}
+		return empNames;
 	}
 	
 	public static void main(String[]args){
 		EmployNameSort ens = new EmployNameSort();
-		ens.addToEmploy("jvv vv", "asdfghj");
-		ens.addToEmploy("fvv vv", "asdfghj");
-		ens.addToEmploy("dvv vv", "asdfghj");
-		ens.addToEmploy("avd vv", "asdfghj");
+		ens.addToEmploy(14, "jvv vv", "asdfghj");
+		ens.addToEmploy(2, "fvv vv", "asdfghj");
+		ens.addToEmploy(5, "dvv vv", "asdfghj");
+		ens.addToEmploy(11, "avd vv", "asdfghj");
 		
 		ens.sortByName();
 		for(Employ emp: ens.sortedEmpList){
