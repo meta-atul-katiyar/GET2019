4fb69e1df249cfbb755b7635b6de514b5196a836
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-08-03 03:19:15 -0700

    Update EmploySet.java

--- DS-4/P-1/EmploySet.java
+++ DS-4/P-1/EmploySet.java
@@ -1,13 +1,34 @@
 
-import java.util.Set;
 import java.util.*;
 
 public class EmploySet{
 
 	Set<Employ> employSet = new HashSet<Employ>();
 
-	public void addToEmploy(String name, String address){
-		Employ employ = new Employ(name, address);
-		employSet.add(employ);
+	public void addToEmploy(int id, String name, String address){
+		int flag = 0;
+		for(Employ emp : employSet){
+			if(emp.getEmpId() == id){
+				flag = 1;
+			}
+		}
+		if(flag == 0){
+			Employ employ = new Employ(id, name, address);
+			employSet.add(employ);
+		}
+	}
+	
+	public static void main(String[]args){
+		EmploySet ens = new EmploySet();
+		ens.addToEmploy(14, "jvv vv", "asdfghj");
+		ens.addToEmploy(2, "fvv vv", "asdfghj");
+		ens.addToEmploy(5, "dvv vv", "asdfghj");
+		ens.addToEmploy(11, "avd vv", "asdfghj");
+		ens.addToEmploy(14, "jvv vv", "asdfghj");
+		
+		for(Employ emp: ens.employSet){
+			System.out.println(emp.getName());
+		}
+		
 	}
 }
