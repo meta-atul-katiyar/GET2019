77e48ff5f24bb05f81209bd13cba1d2cf7adc570
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-08-06 00:11:25 -0700

    Update EmployeeNode.java

--- Algo-1/P-2/EmployeeNode.java
+++ Algo-1/P-2/EmployeeNode.java
@@ -1,20 +1,11 @@
-package question2;
 
-public class EmployeeNode 
-{
-    private Employee employee;
-    EmployeeNode previous;
-    EmployeeNode next;
-    
-    public EmployeeNode(Employee employee)
-    {
-    	this.employee = employee;
-    	next = null;
-    	previous = null;
-    }
-    
-    public Employee getEmployee()
-    {
-    	return employee;
-    }
+public class EmployeeNode {
+
+	Employee emp;
+	EmployeeNode prev = null;
+	EmployeeNode next = null;
+	
+	EmployeeNode(Employee emp){
+		this.emp = emp;
+	}
 }
