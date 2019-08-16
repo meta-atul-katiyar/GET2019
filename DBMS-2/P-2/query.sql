8fb4d0ba6b4b8e56296e56f6cd9ec6da2b37157d
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-08-16 01:16:09 -0700

    Update query.sql

--- DBMS-2/P-2/query.sql
+++ DBMS-2/P-2/query.sql
@@ -1,23 +1,39 @@
-SELECT idproducts, categoryName, unitPrice FROM storefront.products as p natural join storefront.category as c
+// Display Id, Title, Category Title, Price of the products which are Active and recently added products should be at top.
+
+SELECT p.productId, pc.categoryId, p.unitPrice
+FROM storefront.product as p natural join storefront.productCategory as pc
 WHERE status = 1
-ORDER BY addTime DESC;
+ORDER BY p.addTime DESC;
 
 
-SELECT * FROM products 
-WHERE idproducts not in(SELECT idproducts FROM productImages );
+// Display the list of products which don't have any images.
+
+SELECT * FROM product AS P
+WHERE P.productId NOT IN(SELECT PI.productId FROM productImage AS PI );
+
+// Display all Id, Title and Parent Category Title for all the Categories listed, sorted by Parent Category Title and then Category Title. 
+(If Category is top category then Parent Category Title column should display “Top Category” as value.)
+
+SELECT  C.categoryId, C.categoryName,  IFNULL(P.categoryName,'topCategory')
+FROM category as C LEFT OUTER JOIN category as P ON P.categoryId = C.parentCategoryId
+ORDER BY P.categoryName ASC , C.categoryName ASC;
 
 
-SELECT C.idcategory, C.categoryName, (SELECT categoryName FROM category where idcategory = C.parentCategoryId) as topCategory FROM category as C
-ORDER BY C.parentCategoryId ASC , C.idcategory ASC;
+// Display Id, Title, Parent Category Title of all the leaf Categories (categories which are not parent of any other category)
+
+SELECT C.categoryId, C.categoryName FROM category as C WHERE (C.categoryId NOT IN 
+(SELECT distinct parentCategoryId as categoryId FROM category where parentCategoryId <> 0));
 
 
-SELECT C.idcategory, C.categoryName FROM category as C WHERE (C.idcategory NOT IN 
-(SELECT distinct parentCategoryId as idcategory FROM category where parentCategoryId <> 0))
+// Display Product Title, Price & Description which falls into particular category Title (i.e. “Mobile”)
+
+SELECT productId, productDescription, unitPrice 
+FROM product natural join productCategory 
+WHERE categoryId = (SELECT categoryId FROM category WHERE categoryName = "aa");
 
 
-SELECT idproducts, productDescription, unitPrice FROM products WHERE idcategory = (SELECT idcategory FROM category WHERE categoryName = "aa");
+// Display the list of Products whose Quantity on hand (Inventory) is under 50.
 
-
-SELECT * FROM products WHERE quantity > 50;
+SELECT * FROM product WHERE quantity > 50;
 
 
