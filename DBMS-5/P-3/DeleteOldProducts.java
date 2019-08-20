c32134eb56ce722524eac8eccba2d941a6c9435d
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-08-20 03:26:28 -0700

    Update DeleteOldProducts.java

--- DBMS-5/P-3/DeleteOldProducts.java
+++ DBMS-5/P-3/DeleteOldProducts.java
@@ -1,35 +1,19 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
 
+import java.sql.SQLException;
 public class DeleteOldProducts {
 
 	/**
 	 * delete products that are not ordered past 1 year
 	 */
 	public void deleteProduct() {
-		PreparedStatement ps = null;
 		DBConnection dbConn = new DBConnection("storefront");
-		String query = "UPDATE product AS P  SET status = 0 WHERE P.productId NOT IN "
-				+ " (SELECT od.productId FROM orderdetail AS od NATURAL JOIN shopperorder AS so "
-				+ "WHERE so.timestamp > now() - INTERVAL 1 YEAR) AND P.productId >0";
-		Connection connection = dbConn.estabConn();
-		;
+		Query queryObj = new Query();
+		String query = queryObj.getDeleteQuery();
 
 		try {
-			connection = dbConn.estabConn();
-			ps = connection.prepareStatement(query);
-			System.out.println(ps.executeUpdate());
+			System.out.println(dbConn.estabConn(query).executeUpdate());
 		} catch (SQLException e) {
-
 			e.printStackTrace();
-		} finally {
-			try {
-				ps.close();
-				connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
