dd5f6d7b9c14c3bb4b924b409b12cb68b7f2e447
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-08-20 03:26:57 -0700

    Update TopParentCategory.java

--- DBMS-5/P-4/TopParentCategory.java
+++ DBMS-5/P-4/TopParentCategory.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
+
 import java.sql.SQLException;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class TopParentCategory {
@@ -9,34 +8,18 @@ public class TopParentCategory {
 	 * print top parent name their sub category count
 	 */
 	public void topParent() {
-		PreparedStatement ps = null;
 		ResultSet rSet = null;
 		DBConnection dbConn = new DBConnection("storefront");
-		String query = "SELECT C.categoryName, count(C.categoryId) FROM category AS P "
-				+ "INNER JOIN category AS C ON P.parentCategoryId = C.categoryId "
-				+ "WHERE C.parentCategoryId is NULL GROUP BY P.parentCategoryId ORDER BY C.categoryName ";
-
-		Connection connection = dbConn.estabConn();
+		Query queryObj = new Query();
+		String query = queryObj.getCategoryQuery();
 
 		try {
-			connection = dbConn.estabConn();
-			ps = connection.prepareStatement(query);
-			rSet = ps.executeQuery();
+			rSet = dbConn.estabConn(query).executeQuery();
 			while (rSet.next())
 				System.out.println(rSet.getString(1) + " " + rSet.getString(2)
 						+ " ");
-
 		} catch (SQLException e) {
-
 			e.printStackTrace();
-		} finally {
-			try {
-				ps.close();
-				rSet.close();
-				connection.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
