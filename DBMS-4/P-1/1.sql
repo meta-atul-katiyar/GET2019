ddd35a16a03289f53931c3de78d47fdfd878221c
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-08-21 02:19:49 -0700

    Update 1.sql

--- DBMS-4/P-1/1.sql
+++ DBMS-4/P-1/1.sql
@@ -13,27 +13,19 @@ END
 
 DELIMITER ;
 
-
-
-
-
-
-
-
 SELECT no_of_orders(8,2019);
+
+
+
 
 DELIMITER //
 DROP FUNCTION IF EXISTS month_of_max_orders //
 CREATE FUNCTION `storefront`.`month_of_max_orders`( year_value INT ) RETURNS int 
 BEGIN 
- DECLARE number, maximum INT;
- DROP TEMPORARY TABLE IF EXISTS order_number;
- CREATE TEMPORARY TABLE IF NOT EXISTS order_number
-  Select count(SO.shopperorderId) AS numberOfOrders, MONTH(SO.timestamp) AS monthNumber  FROM shopperorder AS SO 
-  WHERE YEAR(timestamp) = year_value GROUP BY MONTH(timestamp);
-  set maximum = (select max(numberOfOrders) FROM order_number);
-( SELECT  monthNumber INTO number FROM order_number  WHERE numberOfOrders = maximum);
-DROP TEMPORARY TABLE IF EXISTS order_number;
+ DECLARE number INT;
+ SET number = (Select MONTH(SO.timestamp) AS monthNumber  FROM shopperorder AS SO 
+  WHERE YEAR(timestamp) = 2019 GROUP BY MONTH(timestamp) 
+  ORDER BY count(SO.shopperorderId) DESC LIMIT 1);
   RETURN number;
 END 
 
