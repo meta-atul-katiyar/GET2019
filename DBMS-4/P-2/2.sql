7c0ccac9aa612bc8f9371c4f366950d89f51e1c0
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-08-21 05:05:15 -0700

    Update 2.sql

--- DBMS-4/P-2/2.sql
+++ DBMS-4/P-2/2.sql
@@ -1,24 +1,21 @@
 
-
 DELIMITER //
 DROP PROCEDURE IF EXISTS avg_sale //
 CREATE PROCEDURE `storefront`.`avg_sale`(month_value INT, year_value INT )
 BEGIN 
  DECLARE number INT;
- SELECT sum(OD.Quantity) INTO number FROM orderdetail AS OD NATURAL JOIN shopperorder AS SO 
- WHERE MONTH(SO.timestamp) = month_value AND YEAR(SO.timestamp) = year_value;
  
  DROP TEMPORARY TABLE IF EXISTS product_sale;
  CREATE TEMPORARY TABLE IF NOT EXISTS product_sale
- SELECT P.productId AS ID, P.productDescription AS prodDes, sum(OD.Quantity) AS quantity FROM 
+ SELECT P.productId AS ID, P.productDescription AS prodDes, sum(OD.Quantity * OD.unitprice) AS sales FROM 
  (product AS P INNER JOIN orderdetail AS OD ON P.productId = OD.productId) 
  INNER JOIN shopperorder AS SO ON SO.shopperorderId = OD.shopperorderId
  WHERE MONTH(SO.timestamp) = month_value AND YEAR(SO.timestamp) = year_value
  GROUP BY MONTH(SO.timestamp), P.productId;
  
- SET number = (SELECT SUM(quantity) FROM product_sale);
+ SET number = (SELECT SUM(sales) FROM product_sale);
  
- SELECT ID, prodDes, Quantity/number AS averageSales FROM product_sale;
+ SELECT ID, prodDes, number/sales AS averageSales FROM product_sale;
  
  DROP TEMPORARY TABLE IF EXISTS product_sale;
   
@@ -29,3 +26,25 @@ END
 DELIMITER ;
 
 call avg_sale(8,2019);
+
+
+
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS order_status //
+CREATE PROCEDURE `storefront`.`order_status`(IN start_date DATE, end_date DATE )
+BEGIN
+  IF(start_date >= end_date) THEN
+   SET start_date = end_date - INTERVAL DAY(end_date) DAY;
+   END IF;
+  Select OD.orderdetailId, OD.productId, OS.status, OS.ondate 
+  FROM orderDetail AS OD NATURAL JOIN orderstatus AS OS
+  WHERE DATE(OS.ondate) >= start_date AND DATE(OS.ondate) <= end_date;
+END 
+
+//
+
+DELIMITER ;
+
+CALL order_status('2019-07-12', '2019-05-31');
+
