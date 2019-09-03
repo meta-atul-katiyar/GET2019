3d90f471350285ad161c6863fb6dee5f0d36d2f3
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-09-03 11:50:51 +0530

    Add files via upload

---
+++ EAD-4/servlet/LogoutServlet.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
+
+import javax.servlet.ServletException;  
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+
+@WebServlet("/Logout")  
+public class LogoutServlet extends HttpServlet {  
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                                throws ServletException, IOException {  
+            response.setContentType("text/html");  
+            PrintWriter out=response.getWriter();  
+            out.print("You are successfully logged out!"
+            		+ "</br></br>"); 
+            request.getRequestDispatcher("login.htm").include(request, response);  
+              
+            HttpSession session=request.getSession();  
+            session.invalidate();  
+              
+            
+              
+            out.close();  
+    }  
+} 
