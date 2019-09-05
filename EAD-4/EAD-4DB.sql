259314b061650aa336d7bbca3c3ef12c7df29ef6
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-09-05 03:18:25 -0700

    Update EAD-4DB.sql

--- EAD-4/EAD-4DB.sql
+++ EAD-4/EAD-4DB.sql
@@ -1,5 +1,4 @@
 
-# Employee details
 CREATE  TABLE IF NOT EXISTS `EAD-4`.`employee_detail` (
 
   `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -22,7 +21,7 @@ CREATE  TABLE IF NOT EXISTS `EAD-4`.`employee_detail` (
 
   );
   
-# vehicle Details  
+  
 CREATE  TABLE IF NOT EXISTS `EAD-4`.`vehicle_detail` (
 
   `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -45,21 +44,19 @@ CREATE  TABLE IF NOT EXISTS `EAD-4`.`vehicle_detail` (
   UNIQUE(ID)
 
   );
-  
-  #ALTER TABLE `ead-4`.`vehicle_detail` CHANGE COLUMN `type` `type` VARCHAR(20) NOT NULL  ;
-
 
   
-  #select * from vehicle_detail;
   
-  # profile picture table
-  CREATE  TABLE IF NOT EXISTS `EAD-4`.`profile_picture` (
+  
+  CREATE  TABLE IF NOT EXISTS `EAD-4`.`profile` (
 
   `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 
-  `image` longblob  NOT NULL ,
+  `image`  VARCHAR(45) NOT NULL ,
   
   `employeeId` INT UNSIGNED NOT NULL  ,
+  
+  `timeOfUpload` DATETIME NOT NULL ,
 
   PRIMARY KEY (`ID`) ,
   FOREIGN KEY(`employeeId`) REFERENCES employee_detail(ID)
@@ -69,14 +66,15 @@ CREATE  TABLE IF NOT EXISTS `EAD-4`.`vehicle_detail` (
 
   );
   
- # select * from pass_details;
+
   
-  # pass Details
+  select * from pass_details;
+  
     CREATE  TABLE IF NOT EXISTS `EAD-4`.`pass_details` (
 
   `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 
-  `amount` INT UNSIGNED NOT NULL,
+  `amount` DOUBLE UNSIGNED NOT NULL,
   
   `planName` VARCHAR(20) NOT NULL, 
   
@@ -92,7 +90,7 @@ CREATE  TABLE IF NOT EXISTS `EAD-4`.`vehicle_detail` (
 
   );
   
-  # friend list
+  
     CREATE  TABLE IF NOT EXISTS `EAD-4`.`friends_list` (
 
   `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -109,7 +107,7 @@ CREATE  TABLE IF NOT EXISTS `EAD-4`.`vehicle_detail` (
 
   );
   
-  # price list
+  
   CREATE  TABLE IF NOT EXISTS `EAD-4`.`price_list` (
 
   `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -130,9 +128,7 @@ CREATE  TABLE IF NOT EXISTS `EAD-4`.`vehicle_detail` (
   INSERT INTO `EAD-4`.`price_list` VALUES(2, "twoWheeler", 10,200,1000);
   INSERT INTO `EAD-4`.`price_list` VALUES(3, "fourWheeler", 20,500,3500);
   
-  select * from Price_list;
-  
-  SELECT * FROM `EAD-4`.`price_list` AS PL;
+
   
   INSERT INTO `EAD-4`.`friends_list` (`userId`, `FriendId`) VALUES (5, 25);
    
@@ -146,15 +142,6 @@ CREATE  TABLE IF NOT EXISTS `EAD-4`.`vehicle_detail` (
   INSERT INTO `EAD-4`.`friends_list` (`userId`, `FriendId`) VALUES (5, 33); 
   INSERT INTO `EAD-4`.`friends_list` (`userId`, `FriendId`) VALUES (5, 34); 
 
-   INSERT INTO `EAD-4`.`employee_detail` VALUES(4,"acv","female","abcz@dfg.com","Aa1234561@",78,"abc");
-  
- 
-  
-  SELECT ED.ID FROM `EAD-4`.`employee_detail` AS ED WHERE ED.email="a@gg.co";
-  SELECT * FROM `EAD-4`.`employee_detail` AS ED;
   
   
-  SELECT * FROM employee_detail;
-  SELECT * FROM vehicle_detail;
-  SELECT * FROM friends_list;
-  SELECT * FROM price_list;
+  
