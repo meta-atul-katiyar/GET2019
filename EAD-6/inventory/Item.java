acc5f50d238a23906a3d053232793802b732d55e
meta-atul-katiyar <atul.katiyar@metacube.com>
2019-09-10 12:05:30 -0700

    EAD-6

---
+++ EAD-6/inventory/Item.java
@@ -0,0 +1,26 @@
+package inventory;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+//@XmlRootElement
+public class Item {
+
+	private String name;
+	private int quantity;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
