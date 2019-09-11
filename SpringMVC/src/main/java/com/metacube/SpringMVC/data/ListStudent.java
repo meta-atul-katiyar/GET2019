4637766cf3663717f3e6f52b707a0985a40067c6
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-09-11 18:42:25 +0530

    Add files via upload

---
+++ SpringMVC/src/main/java/com/metacube/SpringMVC/data/ListStudent.java
@@ -0,0 +1,14 @@
+package com.metacube.SpringMVC.data;
+
+import com.metacube.SpringMVC.model.Student;
+
+/**
+ *Class have student object and add the student.
+ */
+public class ListStudent {	
+		public static final Student s1 = new Student("ab","bc","de","ab@gmail.com","11","21");
+		public static final Student s2 = new Student("ef","gh","ij","ef@gmail.com","12","21");
+		public static final Student s3 = new Student("kl","mn","op","kl@gmail.com","11","21");
+		public static final Student s4 = new Student("qr","st","uv","qr@gmail.com","12","21");
+	
+}
