48881c6cf61329b0c641cb447ad9d3b4eb6bce26
meta-atul-katiyar <52689682+meta-atul-katiyar@users.noreply.github.com>
2019-10-12 18:05:26 +0530

    Delete task-model.spec.ts

--- src/app/task-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TaskModel } from './task-model';
-
-describe('TaskModel', () => {
-  it('should create an instance', () => {
-    // expect(new TaskModel(taskName, taskDescription, taskPriority, status, creationDate, completionDate)).toBeTruthy();
-  });
-});
